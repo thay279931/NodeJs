f5fada9252fa653633ccd5d517745431f43d4bef
Ethen <thay279931@gmail.com>
2022-10-17 09:32:58 +0800

    nodejs

--- routes/address-book.js
+++ routes/address-book.js
@@ -3,32 +3,33 @@ const router = express.Router();
 const db = require(__dirname + '/../modules/db_connect2');
 
 
-router.use((req, res, next) => {
+router.use((req, res, next)=>{
     next();
 });
 
 // CRUD
 
-router.get('/', async (req, res) => {
+router.get(['/', '/list'], async (req, res)=>{
     const perPage = 20;
     let page = +req.query.page || 1;
-    if (page < 1) {
+    if(page<1){
         return res.redirect(req.baseUrl);
     }
     const t_sql = "SELECT COUNT(1) totalRows FROM address_book";
-    const [[{ totalRows }]] = await db.query(t_sql);
+    const [[{totalRows}]] = await db.query(t_sql);
 
     let totalPages = 0;
     let rows = [];
-    if (totalRows > 0) {
-        totalPages = Math.ceil(totalRows / perPage);
-        if (page > totalPages) {
+    if(totalRows>0){
+        totalPages = Math.ceil(totalRows/perPage);
+        if(page>totalPages) {
             return res.redirect(`?page=${totalPages}`);
         }
-        const sql = `SELECT * FROM address_book ORDER BY sid DESC LIMIT ${(page - 1) * perPage}, ${perPage} `;
+        const sql = `SELECT * FROM address_book ORDER BY sid DESC LIMIT ${(page-1)*perPage}, ${perPage} `;
         [rows] = await db.query(sql);
     }
-    res.json({ totalRows, totalPages, perPage, page, rows });
+    // res.json({totalRows, totalPages, perPage, page, rows});
+    res.render('address-book/list', {totalRows, totalPages, perPage, page, rows});
 });
 
 module.exports = router;
